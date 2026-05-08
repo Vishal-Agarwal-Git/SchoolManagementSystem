6d4a4e50d5dac15c3850f7e9e8d484ffbef217f6
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-08 22:27:19 +0530

    Books  model modified

--- SchoolManagementSystem/Models/Books.cs
+++ SchoolManagementSystem/Models/Books.cs
@@ -2,5 +2,12 @@
 {
     public class Books
     {
+        public int Id { get; set; }
+        public string ISBNCode { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string AuthorId { get; set; }
+        
     }
 }
