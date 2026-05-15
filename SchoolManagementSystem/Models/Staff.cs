df43312bd36d770f025ffa858d7d177710dc62aa
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-16 00:01:27 +0530

    .

---
+++ SchoolManagementSystem/Models/Staff.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolManagementSystem.Models
+{
+    public class Staff
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DeaprtmentId { get; set; }
+    }
+}
