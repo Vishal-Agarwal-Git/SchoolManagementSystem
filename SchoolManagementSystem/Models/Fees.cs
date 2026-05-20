98c4b8ae7717c2a8d8247a3fd4ce51fe1badedf9
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-20 23:50:24 +0530

    Fees Method updated

--- SchoolManagementSystem/Models/Fees.cs
+++ SchoolManagementSystem/Models/Fees.cs
@@ -4,7 +4,9 @@
     {
         public int FeesId { get; set; }
         public int StudentId { get; set; }
-        public int CourseId { get; set; }
+        public int FeeAmount { get; set; }
+        public double FeeDeposite { get; set; }
+        public DateTime DepositeDateTime { get; set; }
 
     }
 }
