461ebdd90cb86bf83f7448bf8a603a25b678c537
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-05-21 23:04:38 +0530

    Admission Update

---
+++ SchoolManagementSystem/Models/Admission.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolManagementSystem.Models
+{
+    public class Admission
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string CourseId { get; set; }
+        public int FormFees { get; set; }
+        public DateTime Date_Time { get; set; }
+        public string PaidSourse { get; set; }
+        public int StaffId { get; set; }
+    }
+}
