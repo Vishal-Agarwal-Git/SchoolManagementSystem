78815e84f72a6fed9ccc74797328288fc5b64802
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-06-01 23:52:15 +0530

    Salary Model Updated

--- SchoolManagementSystem/Models/Salary.cs
+++ SchoolManagementSystem/Models/Salary.cs
@@ -3,6 +3,10 @@
     public class Salary
     {
         public int SalaryId { get; set; }
+        public int StaffId { get; set; }
+        public string StaffName { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string SalaryMonth { get; set; }
 
     }
 }
