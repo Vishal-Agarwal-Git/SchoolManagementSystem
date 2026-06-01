12bceca9797f3985d9bd64b5eb4b357bb5cd1fca
Vishal-Agarwal-Git <vishalagar86@gmail.com>
2026-06-02 00:16:27 +0530

    Attendence Model Created

--- SchoolManagementSystem/Models/Attendence.cs
+++ SchoolManagementSystem/Models/Attendence.cs
@@ -2,5 +2,11 @@
 {
     public class Attendence
     {
+        public int Id { get; set; }
+        public int EnrollmentId { get; set; }
+        public DateOnly DateArrival { get; set; }
+        public DateTime TimeArrival { get; set; }
+        public DateOnly DateDepature { get; set; }
+        public DateTime TimeDepature { get; set; }
     }
 }
